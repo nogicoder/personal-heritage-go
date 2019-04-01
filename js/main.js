1f0439d25043d6221318d149d5aa2f8beac1e982
Mai Bach <admin@Admins-MacBook-Air.local>
2019-04-02 05:26:17 +0700

    Waypoint 16

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let defaultContent = $(".main__post-layout__post");
+
+$(document).ready(
+    function() {
+        mHeritageGoService.getPhotos()
+            .then(photos => {
+                mHeritageGoService.getPhoto(photos[1])
+                    .then(photo => {
+                        console.log(photo)
+                    });
+                photos.forEach(photo => {
+                    let content = defaultContent.clone();
+                    photoInfo = mHeritageGoService.getPhoto(photo).then(photo => {
+                            console.log(photo);
+                            content.find(".avatar-img").prop("src", "https:" + photo.account.picture_url).attr("alt", photo.account.fullname);
+                            content.find(".img-description").html(photo.title[0].content);
+                            content.find(".img-location").html(photo.area_name);
+                            if (photo.capture_time) {
+                                content.find(".img-time").html(
+                                    photo.capture_time
+                                );
+                            } else {
+                                content.find(".img-time").html(
+                                    "undefined"
+                                );
+                            };
+                            content.find(".main-img").prop("src", "https:" + photo.image_url + "?size=large");
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        });
+                    content.appendTo($(".main__post-layout"));
+                });
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+);
