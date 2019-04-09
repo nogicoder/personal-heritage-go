395b677f4dcbe6861fad9e4dcaf461bed6f0ddcd
Mai Bach <admin@Admins-MacBook-Air.local>
2019-04-09 20:10:28 +0700

    Project final

--- js/main.js
+++ js/main.js
@@ -1,89 +1,222 @@
-// Show password
-function show() {
+//Showing password
+function showPass() {
     var x = document.getElementById("form-group__password");
     y = document.getElementById("form-group__lock");
+    // showing password when clicking the clock
     if (x.type === "password") {
         x.type = "text";
-        y.className = "fas fa-lock-open";
+        y.className = "fas fa-unlock-alt";
+        // and hide password when clicking again
     } else {
         x.type = "password";
         y.className = "fas fa-lock";
     }
 }
 
-// Fetching photos
-let defaultContent = $("#js-post");
+// ------------------------------------- Fetching Photo -------------------------------------
 
-// Fetching photos
-$(document).ready(
-    function() {
-        mHeritageGoService.getPhotos()
-            .then(photos => {
-                mHeritageGoService.getPhoto(photos[1])
-                    .then(photo => {
-                        console.log(photo)
-                    });
-                photos.forEach(photo => {
-                    let content = defaultContent.clone();
-                    photoInfo = mHeritageGoService.getPhoto(photo).then(photo => {
-                            console.log(photo);
-                            content.find("#js-avatar").prop("src", "https:" + photo.account.picture_url).attr("alt", photo.account.fullname);
-                            content.find("#js-title").html(photo.title[0].content);
-                            content.find("#js-location").html(photo.area_name);
-                            if (photo.capture_time) {
-                                content.find("#js-time").html(
-                                    photo.capture_time
-                                );
-                            } else {
-                                content.find("#js-time").html(
-                                    "undefined"
-                                );
-                            };
-                            content.find("#js-photo").prop("src", "https:" + photo.image_url + "?size=large");
-                            if (photo.like_count) {
-                                content.find("#js-like").html(photo.like_count);
-                            } else {
-                                content.find("#js-like").html("0");
-                            };
-                            if (photo.comment_count) {
-                                content.find("#js-comment").html(photo.comment_count);
-                            } else {
-                                content.find("#js-comment").html("0");
-                            };
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        });
-                    content.appendTo($(".l-post"));
-                });
-            })
-            .catch(error => {
-                console.log(error)
-            });
+// Fetching Photo Details
+function assignToElements(content, photo) {
+
+    // Fetch Author's Avatar
+    content
+        .find("#js-avatar")
+        .prop("src", "https:" + photo.account.picture_url)
+        .attr("alt", photo.account.fullname);
+
+    // Fetch Title
+    content
+        .find("#js-title")
+        .html(photo.title[0].content);
+
+    // Fetch Location
+    content
+        .find("#js-location")
+        .html(photo.area_name);
+
+    // Fetch Capture Time
+    if (photo.capture_time) {
+
+        // If Data Exist
+        content
+            .find("#js-time")
+            .html(photo.capture_time);
+    } else {
+
+        // If Data Not Exist
+        content
+            .find("#js-time")
+            .html("undefined");
     }
-);
 
-// $(window).scroll(() => {
-//     $('.header-body').css({ 'opacity': '.75' });
-// })
+    // Fetch Photo
+    content
+        .find("#js-photo")
+        .prop("src", "https:" + photo.image_url + "?size=large");
 
-// $(function() {
-//     var content = $('.main__post-layout'),
-//         header = $('.container');
+    // Fetch Likes
+    if (photo.like_count) {
+        content
+            .find("#js-like")
+            .html(photo.like_count);
+    } else {
+        content
+            .find("#js-like")
+            .html("0");
+    }
 
-//     $(content).clone().prependTo(header).addClass('blurred');
+    // Fetch Comments
+    if (photo.comment_count) {
+        content
+            .find("#js-comment")
+            .html(photo.comment_count);
+    } else {
+        content
+            .find("#js-comment")
+            .html("0");
+    }
+};
 
-//     $('.blurred').css({
-//         'background': '#fff',
-//         '-webkit-filter': 'blur(.1em)',
-//         'filter': 'blur(.1em)',
-//     });
-// });
+//Translate
+function translate(content, photo) {
 
-// $(document).scroll(function() {
-//     var scroll = $(this).scrollTop();
-//     $('.blurred').css({
-//         '-webkit-transform': 'translateY(-' + scroll + 'px)',
-//         'transform': 'translateY(-' + scroll + 'px)'
-//     });
-// })
+    // translate the caption of the photo
+    let photoId = "",
+        caption = "",
+        locale = "";
+    let transIcon = content.find($(".js-translate-icon"));
+    $(transIcon).click(function() {
+        $(transIcon).removeClass("c-translate-icon--shake");
+    });
+    let langChoice = content.find($(".js-language-choice"));
+    $(langChoice).click(function(item) {
+        item.preventDefault();
+        let lang = item.target.text
+        if (lang === "English") {
+            locale = "eng";
+        } else if (lang === "Vietnamese") {
+            locale = 'vie'
+        }
+        $(transIcon).removeClass("js-translate-icon");
+        $(transIcon).addClass("translate-language");
+        $(transIcon).text(lang);
+        content.find($(".c-post__title")).css("display", "none");
+        content.find($(".c-post__new-title")).css("display", "block");
+
+        let newCaption = content.find($(".c-post__new-title"));
+        newCaption.submit(function(inp) {
+            inp.preventDefault();
+            let photoId = photo.photo_id;
+            let caption = $(newCaption).find($("input")).val();
+            mHeritageGoService.suggestPhotoCaption(photoId, caption, locale).catch(error => {
+                alert(`${caption}\n${error.status}: ${error.statusText}\nServer is coming soon, wait for the great future!`);
+                console.log(error);
+            });
+
+            $(transIcon).addClass("js-translate-icon");
+            $(transIcon).addClass("c-translate-icon--shake");
+            $(transIcon).removeClass("translate-language");
+            $(transIcon).text("");
+
+            content.find($(".c-post__title")).css("display", "block");
+            content.find($(".c-post__new-title")).css("display", "none");
+        });
+        $(document).click(function(event) {
+            if (event.target.tagName.toLowerCase() != 'a' && event.target.tagName.toLowerCase() != 'input' && content.find($(".c-post__new-title")).prop('style')[0] == 'display') {
+                content.find($(".c-post__title")).css("display", "block");
+                content.find($(".c-post__new-title")).css("display", "none");
+                $(transIcon).addClass("js-translate-icon");
+                $(transIcon).addClass("c-translate-icon--shake");
+                $(transIcon).removeClass("translate-language");
+                $(transIcon).text("");
+            }
+        });
+        $(document).scroll(function(event) {
+            content.find($(".c-post__title")).css("display", "block");
+            content.find($(".c-post__new-title")).css("display", "none");
+            $(transIcon).addClass("js-translate-icon");
+            $(transIcon).addClass("c-translate-icon--shake");
+            $(transIcon).removeClass("translate-language");
+            $(transIcon).text("");
+
+        });
+    });
+
+}
+
+
+//Assign photo details to each post
+function assignToPost(photo) {
+    const defaultContent = $("#js-post");
+    let content = defaultContent.clone();
+    mHeritageGoService
+        .getPhoto(photo)
+        .then(photo => {
+            console.log(photo);
+            assignToElements(content, photo);
+            content.appendTo($("main"));
+            translate(content, photo);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+//Get detail of each photo
+function getPhotoDetail(photos) {
+    mHeritageGoService
+        .getPhoto(photos[1])
+        .then(photo => {
+            console.log(photo);
+        });
+    photos.forEach(photo => {
+        assignToPost(photo)
+    });
+}
+
+//Fetching list of Photos from Heritage GO API
+function fetchPhotos() {
+    mHeritageGoService
+        .getPhotos({
+            limit: 10,
+            offset: 5,
+
+        })
+        .then(photos => {
+            getPhotoDetail(photos)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+// Start fetching after the DOM is ready
+$(document).ready(fetchPhotos());
+
+// ------------------------------------- UX FEATURES -------------------------------------
+
+//Auto Load More Photo When Scroll
+$(window).scroll(function() {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        fetchPhotos();
+        $(".l-inner").css("opacity", ".75");
+    }
+});
+
+//Blur Header When Scroll
+$(document).scroll(function() {
+    var scroll = $(this).scrollTop();
+    $(".js-blur").css({
+        '-webkit-transform': 'translateY(-' + scroll + 'px)',
+        'transform': 'translateY(-' + scroll + 'px)',
+    });
+});
+
+$(function() {
+    $("main").clone().prependTo($("header")).addClass("js-blur");
+    $(".js-blur").css({
+        '-webkit-filter': 'blur(.2em)',
+        'filter': 'blur(.2em)',
+        'background': '#fff',
+    });
+});
---
+++ team_agreement.md
@@ -0,0 +1,54 @@
+1. The name of the team: *...* <3 <3 <3 team
+
+
+2. The working hours/days each member commits to: at least 30 hours per week
+      + We have a 15m meeting session everyday
+
+      + The Sprint Planning Meeting  (per week):
+            + first time: *Monday at 4:00pm*
+            + second time: *Wednesday at 3:00pm*
+            + third time: *Friday at 3:00pm*
+
+      + The timetable of each member at INTEK:
+            + dnhu: *after 1:00pm (Mon, Wed, Sun), 10:00am - 6:00pm Tue, Thur*   
+            + mbach:
+            + nthanhvy:
+
+
+3. Team Rule:
+      + On time.
+      + Respect another members opinion
+      + Constructive feedback
+      + Check group chat and reply frequently.
+
+
+4. The strengths and weaknesses of each member, and how to leverage/mitigate them for the project:
+      + dnhu:
+              - strengths: the ability to use the search engine efficiently, fast-learner
+              - weaknesses: having difficulties using English, presentation, code is not clearly
+      + mbach:
+              - strengths: creative, the ability to use the search engine efficiently, fast-learner
+              - weaknesses: unclear code, lack of time
+      + nthanhvy:
+              - strengths:
+              - weaknesses:
+
+
+5. Project Vision:
+
+
+
+6. Product Backlog:
+      + Team Agreement
+      + Way points (20 features)
+      + Code Refactoring
+      + Testing
+      + Deploying
+
+7. Sprints:
+      **1stSprint**
+              + Create GitHub Repo
+              + Waypoints from 1 - 10
+
+8. Workflow:
+      Each person will complete waypoints 1-10 solo. Will merge the codes before each Sprint Review                                                                                                                                                          
